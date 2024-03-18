a684624f12412e13cb0c9731d96e24c3aa85b298
juanagustin0504 <juanagustin@naver.com>
2024-03-18 20:19:32 +0900

    [+] 연산자 + 제어문

---
+++ Mon20240318/src/day20240318/Exam1.java
@@ -0,0 +1,33 @@
+package day20240318;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		/**
+		 * Date: 2024.03.18
+		 * Description: Condition
+		 */
+//		int a = 3;
+//		int b = 10;
+//		
+//		if (a < b) {
+//			System.out.println(a > b); // false
+//			System.out.println(b > a && b < 20); // true
+//		}
+		
+		
+		int x = 6;
+		if (x >= 3 && x <= 6) {
+			System.out.println("A");
+		}
+		
+		if (x >= 3) {
+			if (x <= 6) {
+				System.out.print("A");
+			}
+		}
+		
+		
+	}
+
+}
